e782922e771bd8673431ac667afbbb99c473cb2b
Ryan Dawson <ryan.dawson@alfresco.com>
2017-08-14 17:15:29 +0100

    changes after package changes in starter

--- src/main/java/org/activiti/services/audit/JpaAuditApplication.java
+++ src/main/java/org/activiti/services/audit/JpaAuditApplication.java
@@ -23,14 +23,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaRepositories("org.activiti.services.audit")
+@EntityScan(basePackages = {"org.activiti.services.audit.events","org.activiti.services.audit.events.model"})
+@ComponentScan("org.activiti")
 @EnableBinding(AuditConsumerChannels.class)
 public class JpaAuditApplication implements CommandLineRunner {
 
