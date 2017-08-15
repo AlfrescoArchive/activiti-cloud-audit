808d4033cf62d5f4dc6025c91b9a21c369f7f501
Ryan Dawson <ryan.dawson@alfresco.com>
2017-08-15 14:21:41 +0100

    test push docker

--- src/test/java/org/activiti/cloud/audit/ApplicationTests.java
+++ src/test/java/org/activiti/cloud/audit/ApplicationTests.java
@@ -1,5 +1,6 @@
 package org.activiti.cloud.audit;
 
+import org.actviti.cloud.audit.JpaAuditApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +8,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = JpaAuditApplication.class)
 @DirtiesContext
 public class ApplicationTests {
 
