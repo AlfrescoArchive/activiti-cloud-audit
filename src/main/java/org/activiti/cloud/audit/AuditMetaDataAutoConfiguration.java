7998c40bef993a4cbecb0a31f2028787ed67d674
Salaboy <salaboy@gmail.com>
2018-04-07 10:47:03 +0100

    adding metadata auto config

--- src/main/java/org/activiti/cloud/audit/AuditMetaDataAutoConfiguration.java
+++ src/main/java/org/activiti/cloud/audit/AuditMetaDataAutoConfiguration.java
@@ -3,24 +3,42 @@ package org.activiti.cloud.audit;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import com.netflix.appinfo.ApplicationInfoManager;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(ApplicationInfoManager.class)
+// This code needs to live here until we find the right abstraction for registering/updating Service Metadata
 public class AuditMetaDataAutoConfiguration implements BeanClassLoaderAware {
 
-    // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
+    @Value("${activiti.cloud.application.name:}")
+    private String applicationName;
+
     @Autowired
-    public AuditMetaDataAutoConfiguration(ApplicationInfoManager appInfoManager) {
+    private ApplicationInfoManager appInfoManager;
+
+
+
+    public AuditMetaDataAutoConfiguration() {
+
+    }
+
+    @PostConstruct
+    public void init(){
         Map<String, String> metadata = new HashMap<>();
         metadata.put("activiti-cloud-service-type",
                      "audit");
+        metadata.put("activiti-cloud-application-name",
+                     applicationName);
         appInfoManager.registerAppMetadata(metadata);
     }
+
 
     public void setBeanClassLoader(ClassLoader classLoader) {
         // Do nothing for now
