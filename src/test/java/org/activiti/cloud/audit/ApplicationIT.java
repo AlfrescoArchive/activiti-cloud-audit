9f3bab3e45c2bab01296310aa2f397db8ef981f4
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-16 14:04:01 +0100

    clean up imports

--- src/test/java/org/activiti/cloud/audit/ApplicationIT.java
+++ src/test/java/org/activiti/cloud/audit/ApplicationIT.java
@@ -1,10 +1,7 @@
 package org.activiti.cloud.audit;
 
-import org.activiti.cloud.services.metadata.MetadataService;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,7 +13,7 @@ public class ApplicationIT {
 
 	@Test
 	public void contextLoads() throws Exception {
-		
+
 	}
 
 }
